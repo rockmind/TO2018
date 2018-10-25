bec81f5938f9604f8802fe401b761ee4efe687b9
Błażej Szulc <blaszu@agora.pl>
2018-10-25 02:47:10 +0200

    koniec dnia

--- src/pl/edu/agh/dronka/shop/model/filter/ItemFilter.java
+++ src/pl/edu/agh/dronka/shop/model/filter/ItemFilter.java
@@ -1,14 +1,19 @@
 package pl.edu.agh.dronka.shop.model.filter;
 
 import pl.edu.agh.dronka.shop.model.Item;
+import pl.edu.agh.dronka.shop.model.ItemBook;
 import pl.edu.agh.dronka.shop.model.ItemDefault;
+import pl.edu.agh.dronka.shop.model.ItemElectronics;
+import pl.edu.agh.dronka.shop.model.ItemMusic;
 
 public class ItemFilter {
-
 	private Item itemSpec = (ItemDefault) new ItemDefault();
 
 	public Item getItemSpec() {
 		return itemSpec;
+	}
+	public void setItemSpec(Item itemSpec) {
+		this.itemSpec = itemSpec;
 	}
 	public boolean appliesTo(Item item) {
 		if (itemSpec.getName() != null
@@ -29,6 +34,50 @@ public class ItemFilter {
 		if (itemSpec.isPolish() && !item.isPolish()) {
 			return false;
 		}
+//		switch (itemSpec.getCategory()) {
+//		case BOOKS:
+//			break;
+//		case ELECTRONICS:
+////			// applies filter only if the flag (warranty) is true)
+////			if (item instanceof ItemElectronics && itemSpec instanceof ItemElectronics
+////					&& ((ItemElectronics) itemSpec).isWarranty() && !((ItemElectronics) item).isWarranty()) {
+////				return false;
+////			}
+////			// applies filter only if the flag (mobile) is true)
+////			if (  item instanceof ItemElectronics && itemSpec instanceof ItemElectronics
+////				  && ((ItemElectronics) itemSpec).isMobile() && !((ItemElectronics) item).isMobile()) {
+////				return false;
+////			}
+//			break;
+//		case FOOD:
+//			break;
+//		case MUSIC:
+//			break;
+//		case SPORT:
+//			break;
+//		}
+
+		
+//		// applies filter for books
+//		if ( !(itemSpec instanceof ItemBook)) {
+//			return false;
+//		}
+//		// applies filter only if the flag (hardcover) is true)
+//		if (  itemSpec instanceof ItemBook
+//			  && ((ItemBook) itemSpec).isHardcover()
+//			  && !((ItemBook) item).isHardcover()) {
+//			return false;
+//		}
+//		// applies filter for musics
+//		if ( !(itemSpec instanceof ItemMusic)) {
+//			return false;
+//		}
+//		//		// applies filter only if the flag (hardcover) is true)
+//		if (  itemSpec instanceof ItemMusic
+//			  && ((ItemMusic) itemSpec).isVideo()
+//			  && !((ItemMusic) item).isVideo()) {
+//			return false;
+//		}
 
 		return true;
 	}
--- src/pl/edu/agh/dronka/shop/view/PropertiesPanel.java
+++ src/pl/edu/agh/dronka/shop/view/PropertiesPanel.java
@@ -8,6 +8,9 @@ import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 import pl.edu.agh.dronka.shop.controller.ShopController;
+import pl.edu.agh.dronka.shop.model.ItemBook;
+import pl.edu.agh.dronka.shop.model.ItemElectronics;
+import pl.edu.agh.dronka.shop.model.ItemMusic;
 import pl.edu.agh.dronka.shop.model.filter.ItemFilter;
 
 public class PropertiesPanel extends JPanel {
@@ -26,6 +29,7 @@ public class PropertiesPanel extends JPanel {
 		removeAll();
 
 		filter.getItemSpec().setCategory(shopController.getCurrentCategory());
+		//TUTAJ Zamienić miejscami góra dół
 		add(createPropertyCheckbox("Tanie bo polskie", new ActionListener() {
 
 			@Override
@@ -45,6 +49,52 @@ public class PropertiesPanel extends JPanel {
 				shopController.filterItems(filter);
 			}
 		}));
+		switch (shopController.getCurrentCategory()) {
+		case BOOKS:
+			add(createPropertyCheckbox("Twarda oprawa", new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					filter.setItemSpec(new ItemBook());
+					((ItemBook) filter.getItemSpec()).setHardcover(
+							((JCheckBox) event.getSource()).isSelected());
+					shopController.filterItems(filter);
+				}
+			}));
+			break;
+		case ELECTRONICS:
+			add(createPropertyCheckbox("Mobilny", new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					filter.setItemSpec(new ItemElectronics());
+					((ItemElectronics) filter.getItemSpec()).setMobile(
+							((JCheckBox) event.getSource()).isSelected());
+					shopController.filterItems(filter);
+				}
+			}));
+			add(createPropertyCheckbox("Gwarancja", new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					filter.setItemSpec(new ItemElectronics());
+					((ItemElectronics) filter.getItemSpec()).setWarranty(
+							((JCheckBox) event.getSource()).isSelected());
+					shopController.filterItems(filter);
+				}
+			}));
+			break;
+		case MUSIC:
+			add(createPropertyCheckbox("Gwarancja", new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					filter.setItemSpec(new ItemMusic());
+					((ItemMusic) filter.getItemSpec()).setVideo(
+							((JCheckBox) event.getSource()).isSelected());
+					shopController.filterItems(filter);
+				}
+			}));
+			break;
+		default:
+			break;
+		}
 
 	}
 
